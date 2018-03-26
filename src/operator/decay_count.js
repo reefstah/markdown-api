4d3bfcdafd536d67c6cd06954569f1701c178a61
reef <public@ralph.tech>
2018-03-26 06:46:37 +0200

    docs decay operator

--- src/operator/decay_count.js
+++ src/operator/decay_count.js
@@ -1,5 +1,12 @@
 import {Observable} from "rxjs/Observable";
 
+/**
+ * Mix between a queue and a buffer, used for parsing large files.
+ * The operator releases the entire queue every time.
+ * Near completion the queue will shrink until completion.
+ * @param bufferSize
+ * @returns {function(*): *}
+ */
 export function decayCount(bufferSize) {
     return (source) => Observable.create(subscriber => {
 
