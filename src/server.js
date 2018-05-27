33e99a0a50af95f3e19f5c4fa9abed9785722552
reef <public@ralph.tech>
2018-05-27 17:56:15 +0200

    focus on config file while reading dirs

--- src/server.js
+++ src/server.js
@@ -6,15 +6,19 @@ import path from 'path';
 import {Observable} from 'rxjs/Observable';
 
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/bindNodeCallback';
+
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/expand';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/concatAll';
 import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/concatMap';
 
 
 export class Server {
@@ -25,21 +29,26 @@ export class Server {
     }
 
     start() {
-        readDirTree(this.dirPath, 2)
-            .subscribe(console.log);
+        getDirConfigs(this.dirPath, 5)
+            .subscribe(console.debug);
     }
 }
 
 
-function readDirTree(dir, deep) {
+function getDirConfigs(rootDir, deep) {
 
     return Observable
-        .bindNodeCallback(fs.readdir)(dir)
-        .map(fileNameList => Observable.from(fileNameList))
-        .concatAll()
-        .map(name => path.resolve(dir, name))
-        .map(dirPath => Observable.bindNodeCallback(fs.stat)(dirPath).filter(stats => stats.isDirectory()).map(bool => dirPath))
-        .concatAll()
-        .map(dirPath => 1 < deep ? readDirTree(dirPath, deep - 1).merge(Observable.of(dirPath)) : Observable.of(dirPath))
-        .concatAll()
+        .bindNodeCallback(fs.readdir)(rootDir)
+        .concatMap(fileNameList => Observable.from(fileNameList))
+        .map(
+            name => {
+                const dirPath = path.resolve(rootDir, name);
+                return {path: dirPath, config: `${dirPath}/${name}.json`};
+            })
+        .concatMap(dirConfig => Observable.bindNodeCallback(fs.stat)(dirConfig.path).filter(stats => stats.isDirectory()).map(bool => dirConfig))
+        .concatMap(
+            dirConfig => 1 < deep ?
+                getDirConfigs(dirConfig.path, deep - 1).merge(Observable.of(dirConfig)) :
+                Observable.of(dirConfig))
+        .filter(dirConfig => fs.existsSync(dirConfig.config));
 }
