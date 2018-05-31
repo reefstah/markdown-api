254f9992bee9a823b0ab63da7d4a710c8bab921b
reef <public@ralph.tech>
2018-05-31 08:35:03 +0200

    moving server into named functions

--- src/server.js
+++ src/server.js
@@ -6,22 +6,17 @@ import path from 'path';
 import {Observable} from 'rxjs/Observable';
 
 import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/zip';
 import 'rxjs/add/observable/bindNodeCallback';
 
-import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/expand';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/concatAll';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/concatMap';
 import 'rxjs/add/operator/reduce';
 import 'rxjs/add/operator/repeat';
+import 'rxjs/add/operator/do';
 
 import {MarkdownReader} from "./markdown_reader";
 
@@ -34,27 +29,20 @@ export class Server {
 
     start() {
 
-        this.getResourceConfigs(this.dirPath, 5)
+        this.getConfigs(this.dirPath, this.dirPath, 5)
+            .do(console.log)
             .reduce(
-                (app, resourceConfig) =>
-                    app.get(resourceConfig.path, (req, res) => this.controller(req, res, resourceConfig)),
+                (app, config) =>
+                    app.get(config.path, (req, res) => this.controller(req, res, config)),
                 express()
             )
             .subscribe(app => app.listen(9000));
     }
 
-    controller(req, res, resourceConfig) {
+    controller(req, res, config) {
 
-        let filePathObs = Observable
-            .bindNodeCallback(fs.readdir)(resourceConfig.source)
-            .concatMap(fileNameList => Observable.from(fileNameList))
-            .filter(name => name.endsWith('.md') && name !== 'README.md')
-            .map(name => `${resourceConfig.source}/${name}`);
-
-        let formatObs = Observable
-            .bindNodeCallback(fs.readFile)(resourceConfig.format)
-            .map(buff => JSON.parse(buff))
-            .repeat();
+        const filePathObs = getFilesObs(config.source);
+        const formatObs = getFormatObs(config.format);
 
         Observable
             .zip(filePathObs, formatObs)
@@ -62,31 +50,60 @@ export class Server {
             .concatMap(reader => reader.read())
             .reduce((acc, value) => acc.concat(value), [])
             .subscribe(result => res.send(result));
+
+
+        function getFilesObs(source) {
+            return Observable
+                .bindNodeCallback(fs.readdir)(source)
+                .concatMap(fileNameList => Observable.from(fileNameList))
+                .filter(name => name.endsWith('.md') && name !== 'README.md')
+                .map(name => `${source}/${name}`);
+        }
+
+        function getFormatObs(format) {
+            return Observable
+                .bindNodeCallback(fs.readFile)(format)
+                .map(buff => JSON.parse(buff))
+                .repeat();
+        }
     }
 
 
-    getResourceConfigs(rootDir, deep) {
+    getConfigs(current, root, deep) {
 
         return Observable
-            .bindNodeCallback(fs.readdir)(rootDir)
+            .bindNodeCallback(fs.readdir)(current)
             .concatMap(fileNameList => Observable.from(fileNameList))
-            .map(
-                name => {
-                    const dir = path.resolve(rootDir, name);
-                    const resourcePath = dir.replace(this.dirPath, '');
+            .map(name => createConfig(name, current, root))
+            .concatMap(filterDirsAsync)
+            // This might be over-engineered, considering removal
+            .concatMap(config => decentDir(config, deep, this.getConfigs))
+            .filter(config => fs.existsSync(config.format));
 
-                    return {
-                        source: dir,
-                        format: `${dir}/${name}.format.json`,
-                        path: resourcePath
-                    };
-                })
-            .concatMap(resourceConfig => Observable.bindNodeCallback(fs.stat)(resourceConfig.source).filter(stats => stats.isDirectory()).map(bool => resourceConfig))
-            .concatMap(
-                resourceConfig => 1 < deep ?
-                    this.getResourceConfigs(resourceConfig.source, deep - 1).merge(Observable.of(resourceConfig)) :
-                    Observable.of(resourceConfig)
-            )
-            .filter(resourceConfig => fs.existsSync(resourceConfig.format));
+
+        function createConfig(name, current, root) {
+
+            const dir = path.resolve(current, name);
+            const resourcePath = dir.replace(root, '');
+
+            return {
+                source: dir,
+                format: `${dir}/${name}.format.json`,
+                path: resourcePath
+            };
+        }
+
+        function filterDirsAsync(config) {
+            return Observable
+                .bindNodeCallback(fs.stat)(config.source)
+                .filter(stats => stats.isDirectory())
+                .map(() => config);
+        }
+
+        function decentDir(config, deep, recursiveFunc, root) {
+            return 1 < deep ?
+                recursiveFunc(config.source, root, deep - 1).merge(Observable.of(config)) :
+                Observable.of(config);
+        }
     }
 }
